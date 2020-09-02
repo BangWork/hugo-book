a0d4cc752343376a193ed052dbe2e2563367d990
binavid chen <binavid.chen@gmail.com>
2020-09-02 11:01:01 +0800

    fix: menu style error

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,15 @@
 (function () {
+    var menu = document.querySelector("aside.book-menu nav")
+    var toc = document.querySelector(".book-page-aside-inner")
+    if (document.readyState === 'complete') {
+        changeMenuHeight();
+    } else {
+        document.addEventListener("DOMContentLoaded", changeMenuHeight);
+    }
+    addEventListener("resize",changeMenuHeight)
+
     addEventListener("scroll", function (event) {
-        var menu = document.querySelector("aside.book-menu nav")
-        var toc = document.querySelector(".book-page-aside-inner")
-        var main = document.querySelector("main.container")
-        var scrollTop = window.scrollY
-        if (scrollTop > 80) {
-            menu.classList.add("fixed")
-            toc.classList.add("fixed")
-        } else {
-            menu.classList.remove("fixed")
-            toc.classList.remove("fixed")
-        }
+        changeMenuHeight()
         tippy.hideAll()
     })
     tippy("[for='toc-control']", {
@@ -28,4 +27,29 @@
         var searchControl = document.querySelector("#search-control");
         searchControl.checked = true;
     })
+    function unsetHeight(){
+        menu.style.height = "unset"
+        toc.style.height = "unset"
+    }
+    function setHeightToFit(bodyHeight,headerHeight){
+        menu.style.height = (bodyHeight - headerHeight + window.pageYOffset)+"px"
+        toc.style.height = (bodyHeight - headerHeight + window.pageYOffset)+"px"
+    }
+    function changeMenuHeight(){
+        var bodyRect = document.body.getBoundingClientRect();
+        var headerHeight = document.querySelector(".book-header").getBoundingClientRect()["height"]
+        if(bodyRect.width < 1024){
+            unsetHeight()
+        }else{
+            if (window.pageYOffset > 80) {
+                unsetHeight()
+                menu.classList.add("fixed")
+                toc.classList.add("fixed")
+            } else {
+                setHeightToFit(bodyRect.height,headerHeight)
+                menu.classList.remove("fixed")
+                toc.classList.remove("fixed")
+            }
+        }
+    }
 }())

6d102ada9b68f0e1374111b7d085d56c2280faa4
彭振聪 <pengzhencong@pengzhencongdeMacBook-Pro.local>
2020-09-17 02:15:32 +0800

    fix: ignore

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,8 +1,5 @@
 module.exports = {
     plugins: [
-        require('autoprefixer')({
-            'overrideBrowserslist': ['chrome>=5', 'safari>=5.5'],
-            'cascade': false
-        })
+        require('autoprefixer')
     ]
 };

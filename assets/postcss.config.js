542714941f82e72cc26d9f6ff7f651ea445430a1
彭振聪 <pengzhencong@pengzhencongdeMacBook-Pro.local>
2020-09-16 18:58:08 +0800

    fix: js格式错误

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    plugins: [
-        require('autoprefixer')({
-            overrideBrowserslist: ['chrome >= 5', 'safari >= 5.5'],
+    plugins: [{
+        'autoprefixer': {
+            overrideBrowserslist: ['chrome>=5', 'safari>=5.5'],
             cascade: false
-        })
-    ]
+        }
+    }]
 }
